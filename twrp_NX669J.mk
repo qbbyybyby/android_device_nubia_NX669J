5ae493e6e53e90dc06fd967afd0d32ab70a17296
qbbyybyby <87138419+qbbyybyby@users.noreply.github.com>
2023-06-01 01:42:34 +0700

    Update twrp_NX669J.mk

--- twrp_NX669J.mk
+++ twrp_NX669J.mk
@@ -6,17 +6,16 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from NX669J device
 $(call inherit-product, device/nubia/NX669J/device.mk)
 
 PRODUCT_DEVICE := NX669J
-PRODUCT_NAME := omni_NX669J
+PRODUCT_NAME := twrp_NX669J
 PRODUCT_BRAND := nubia
 PRODUCT_MODEL := NX669J
 PRODUCT_MANUFACTURER := nubia
